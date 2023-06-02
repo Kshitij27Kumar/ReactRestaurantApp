15950b99841a94380906ae8e560e63156940762a
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-06-02 17:16:34 +0530

    Routes created

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Menu from './Menu'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Contact from './Contact'
+import About from './About'
 const Body = () => {
   return (
     <div>
-      <Menu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   )
 }
