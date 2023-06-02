84681dac07d0edea73a85e9f503f5dbac4ac51c1
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-06-02 12:53:17 +0530

    Dish Detail Component added

--- src/components/body/DishDetail.jsx
+++ src/components/body/DishDetail.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-const MenuItem = (props) => {
+const DishDetail = (props) => {
   return (
     <div>
-      <Card style={{ width: '25rem' }}>
+      <Card style={{ width: '35rem' }}>
         <Card.Img variant='top' src={props.dish.image} />
         <Card.Body>
-          <Card.Title
-            style={{ cursor: 'pointer' }}
-            onClick={() => props.DishSelect(props.dish)}
-          >
-            {props.dish.name}
-          </Card.Title>
+          <Card.Title>{props.dish.name}</Card.Title>
           <Card.Text>{props.dish.description}</Card.Text>
           <p>
             <b>Price: {props.dish.price}</b>
@@ -19,10 +14,13 @@ const MenuItem = (props) => {
           <p>
             <b>Label: {props.dish.label}</b>
           </p>
+          <p>
+            <b>Category: {props.dish.category}</b>
+          </p>
         </Card.Body>
       </Card>
     </div>
   )
 }
 
-export default MenuItem
+export default DishDetail
--- src/components/body/Menu.jsx
+++ src/components/body/Menu.jsx
@@ -1,21 +1,34 @@
 import React, { Component } from 'react'
 import DISHES from '../../data/dishes.js'
 import MenuItem from './MenuItem.jsx'
+import DishDetail from './DishDetail.jsx'
 export class Menu extends Component {
   state = {
     dishes: DISHES,
+    selectedDish: false,
   }
+
+  onDishSelect = (dish) => {
+    // console.log(dish)
+    this.setState({ selectedDish: dish })
+  }
+
   render() {
     const menu = this.state.dishes.map((item) => {
-      return <MenuItem dish={item} key={item.id}></MenuItem>
+      return (
+        <MenuItem dish={item} key={item.id} DishSelect={this.onDishSelect} />
+      )
     })
+
+    let dishDetails = null
+    if (this.state.selectedDish != null) {
+      dishDetails = <DishDetail dish={this.state.selectedDish} />
+    }
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-6'>{menu}</div>
-          <div className='col-6'>
-            <h1>Col 6 Right</h1>
-          </div>
+          <div className='col-6'>{dishDetails}</div>
         </div>
       </div>
     )
